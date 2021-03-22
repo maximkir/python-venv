baf54247f3ff0dbf502675719708b555bb64ef35
Maxim Kirilov <kir.maxim@gmail.com>
2021-03-22 22:52:22 +0200

    wip

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,8 +1,14 @@
 import os
+import pip
 import platform
 
-EXPECTED_VERSION = os.getenv("PYTHON_VERSION")
+EXPECTED_PYTHON_VERSION = os.getenv("PYTHON_VERSION")
+EXPECTED_PIP_VERSION = os.getenv("PIP_VERSION")
 
 
-def test_version():
-    assert platform.python_version() == EXPECTED_VERSION
+def test_python_version():
+    assert platform.python_version() == EXPECTED_PYTHON_VERSION
+
+
+def test_pip_version():
+    assert pip.__version__ == EXPECTED_PIP_VERSION
